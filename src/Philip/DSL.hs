7bf06438ec0944af342d391920971b41a5f1371a
Fujimura Daisuke <me@fujimuradaisuke.com>
2013-11-16 21:30:26 +0900

    Add writer for field which expects string, and PackageIdentifier

--- src/Philip/DSL.hs
+++ src/Philip/DSL.hs
@@ -1,11 +1,26 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module Philip.DSL
     ( desc
     , author
     , copyright
     , maintainer
+    , name
+    , version
+    , licenseFile
+    , stability
+    , homepage
+    , packageUrl
+    , bugReports
+    , synopsis
+    , description
+    , category
     ) where
 
 import           Control.Monad.State
+import           Data.List.Split                 (splitOn)
+import           Data.Version                    (Version (..))
+import           Distribution.Package            (PackageIdentifier (..),
+                                                  PackageName (..))
 import           Distribution.PackageDescription (PackageDescription,
                                                   emptyPackageDescription)
 import qualified Distribution.PackageDescription as PD
@@ -13,7 +28,26 @@ import qualified Distribution.PackageDescription as PD
 desc :: State PackageDescription a -> PackageDescription
 desc = flip execState emptyPackageDescription
 
-author, copyright, maintainer :: String -> State PackageDescription ()
-author x     = state $ \pd -> ((), pd { PD.author     = x })
-copyright x  = state $ \pd -> ((), pd { PD.copyright  = x })
-maintainer x = state $ \pd -> ((), pd { PD.maintainer = x })
+author, copyright, maintainer, version, name, licenseFile, stability,
+  homepage, packageUrl, bugReports, synopsis, description, category
+  :: String -> State PackageDescription ()
+author x      = state $ \pd -> ((), pd { PD.author      = x })
+copyright x   = state $ \pd -> ((), pd { PD.copyright   = x })
+maintainer x  = state $ \pd -> ((), pd { PD.maintainer  = x })
+licenseFile x = state $ \pd -> ((), pd { PD.licenseFile = x })
+stability x   = state $ \pd -> ((), pd { PD.stability   = x })
+homepage x    = state $ \pd -> ((), pd { PD.homepage    = x })
+packageUrl x  = state $ \pd -> ((), pd { PD.pkgUrl      = x })
+bugReports x  = state $ \pd -> ((), pd { PD.bugReports  = x })
+synopsis x    = state $ \pd -> ((), pd { PD.synopsis    = x })
+description x = state $ \pd -> ((), pd { PD.description = x })
+category x    = state $ \pd -> ((), pd { PD.category    = x })
+
+version x    = state $ \pd -> ((), pd { PD.package    = update x (PD.package pd) })
+  where
+    -- TODO Update version tags
+    update v PackageIdentifier{pkgName} = PackageIdentifier pkgName $ Version (parse v) []
+    parse xs = map (\x -> read x :: Int) $ splitOn "." xs
+name x = state $ \pd -> ((), pd { PD.package = update x (PD.package pd) })
+  where
+    update x PackageIdentifier{pkgVersion} = PackageIdentifier (PackageName x) pkgVersion
--- test/PhilipSpec.hs
+++ test/PhilipSpec.hs
@@ -1,9 +1,13 @@
 module PhilipSpec (main, spec) where
-import Philip.DSL
+import           Philip.DSL
+
 import           Control.Monad.State
+import           Data.Version                    (Version (..))
+import           Distribution.Package            (PackageIdentifier (..),
+                                                  PackageName (..))
 import qualified Distribution.PackageDescription as PD
 
-import Test.Hspec
+import           Test.Hspec
 
 main :: IO ()
 main = hspec spec
@@ -14,9 +18,18 @@ spec = do
     it "should work fine" $ do
       let me = "Fujimura Daisuke"
       let d = desc $ do
+            name "foo"
+            version "1.2.3"
             author me
             maintainer me
             copyright $ "(c) 2013 " ++ me
+            licenseFile "LICENSE"
+            stability "stable"
+
+
       PD.author d `shouldBe` "Fujimura Daisuke"
       PD.maintainer d `shouldBe` "Fujimura Daisuke"
       PD.copyright d `shouldBe` "(c) 2013 Fujimura Daisuke"
+      PD.package d `shouldBe` PackageIdentifier (PackageName "foo") (Version [1,2,3] [])
+      PD.licenseFile d `shouldBe` "LICENSE"
+      PD.stability d `shouldBe` "stable"
